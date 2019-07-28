b87f62829562dca58e844d911879878b1e56539a
Gustavo Gomes <gustavo.sgomess@gmail.com>
2019-07-28 00:36:07 -0300

    Voltando os requestmapping

--- src/main/java/magiccards/controllers/ExpansionsController.java
+++ src/main/java/magiccards/controllers/ExpansionsController.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +21,12 @@ public class ExpansionsController {
 	@Autowired
 	private ExpansionRepository expansionRepository;
 
-	@GetMapping(value = "{id}")
+	@RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
 	public Expansion getExpansionById(@PathVariable("id") Integer expansionId) {
 		return expansionRepository.findOne(expansionId);
 	}
 
-	@GetMapping
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
 	public Page<Expansion> getExpansions(@RequestParam(value="page")int pageNumber, 
 										 @RequestParam int size) {
 
@@ -39,17 +35,17 @@ public class ExpansionsController {
 		return expansionRepository.findAll(page);
 	}
 
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json")
 	public void create(@RequestBody Expansion expansion) {
 		expansionRepository.save(expansion);
 	}
 
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json")
 	public void update(@RequestBody Expansion expansion) {
 		expansionRepository.save(expansion);
 	}
 
-	@DeleteMapping(value = "{id}")
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable("id")Integer id) {
 		expansionRepository.delete(id);
 	}
